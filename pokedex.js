ec55a37ffd06d7058246dfd8c9910483dee936b7
David Williams <willidv@gmail.com>
2017-08-31 11:01:54 -0500

    pokedex

---
+++ pokedex.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    for(i = 0; i <=151; i ++){  
+        $("#pokemon").append("<img src='http://pokeapi.co/media/img/" + i +
+        ".png'>");          
+    }
+});
+
+
